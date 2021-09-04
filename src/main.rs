c3fff1de40051be8518b028758a91ab513acd385
Yuki Kodama <endflow.net@gmail.com>
2021-09-04 15:37:07 +0900

    sort row

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,20 @@ fn main() {
         sort_column(&mut buf, col);
     }
 
+    for row in 0..buf.height() {
+        sort_row(&mut buf, row);
+    }
+
     buf.save("sorted.png").unwrap();
 }
 
 fn sort_column(buf: &mut image::RgbImage, x: u32) {
+    let height = buf.height();
+
     let mut y = 0;
     let mut y_end = 0;
 
-    while y_end < buf.height() - 1 {
+    while y_end < height - 1 {
         y = match finder::get_first_not_black_y(buf, x, y, BLACK) {
             Some(y) => y,
             _ => break,
@@ -41,6 +47,39 @@ fn sort_column(buf: &mut image::RgbImage, x: u32) {
         }
 
         y = y_end + 1;
+    }
+}
+
+fn sort_row(buf: &mut image::RgbImage, y: u32) {
+    let width = buf.width();
+
+    let mut x = 0;
+    let mut x_end = 0;
+
+    while x_end < width - 1 {
+        x = match finder::get_first_not_black_x(buf, x, y, BLACK) {
+            Some(x) => x,
+            _ => break,
+        };
+
+        x_end = finder::get_next_black_x(buf, x, y, BLACK);
+
+        let len = x_end - x;
+        let mut line = Vec::with_capacity(len as usize);
+
+        for x in x..x_end {
+            let pixel = buf.get_pixel(x, y);
+            line.push(*pixel);
+        }
+
+        line.sort_by(|a, b| to_gray(a).cmp(&to_gray(b)));
+
+        for i in 0..len {
+            let pixel = line.get(i as usize).unwrap();
+            buf.put_pixel(x + i, y, *pixel);
+        }
+
+        x = x_end + 1;
     }
 }
 
