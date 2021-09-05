5fa40cdc43ee1f1d4255fb022e6704a385ebb08f
Yuki Kodama <endflow.net@gmail.com>
2021-09-06 01:43:33 +0900

    use once cell

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use asdf_pixel_sort::sort;
+use asdf_pixel_sort::{sort, PColor};
+use once_cell::sync::Lazy;
 
-const BLACK: u32 = 48;
+static BLACK: Lazy<PColor> = Lazy::new(|| PColor::new(11, 220, 0));
 
 fn main() {
     let img = image::open("ASDFPixelSort/pic.jpg").unwrap();
     let mut buf = img.to_rgb8();
 
-    sort(&mut buf, BLACK);
+    sort(&mut buf, &BLACK);
 
     buf.save("sorted.png").unwrap();
 }
