403a94982fb3dec782d48c21b05ace40c601ad01
Yuki Kodama <endflow.net@gmail.com>
2021-09-06 23:13:13 +0900

    follow new API

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use asdf_pixel_sort::{sort, PColor};
-use once_cell::sync::Lazy;
-
-static BLACK: Lazy<PColor> = Lazy::new(|| PColor::new(11, 220, 0));
+use asdf_pixel_sort::{sort_with_options, Mode, Options};
 
 fn main() {
     let img = image::open("ASDFPixelSort/pic.jpg").unwrap();
     let mut buf = img.to_rgb8();
 
-    sort(&mut buf, &BLACK);
+    let options = Options {
+        mode: Mode::black(),
+    };
+    sort_with_options(&mut buf, &options);
 
     buf.save("sorted.png").unwrap();
 }
